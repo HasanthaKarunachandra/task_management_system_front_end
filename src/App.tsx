57046a46cce15bc3f7b07af488218673dfbdcd8f
Hasantha Karunachandra <hkarunachandra@gmail.com>
2024-03-22 03:57:59 +0530

    routing added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+import {BrowserRouter as Router, Routes,Route, Link} from "react-router-dom";
 import './App.css'
+import Employee from "./component/Employee.tsx";
+import Project from "./component/Project.tsx";
+import Task from "./component/Task.tsx";
 
 function App() {
   return(
-      <h1>Hellow World</h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Employee</Link>
+              </li>
+              <li>
+                <Link to='/project'>Project</Link>
+              </li>
+              <li>
+                <Link to='/task'>Task</Link>
+              </li>
+            </ul>
+          </nav>
+          <hr/>
+
+          <Routes>
+            <Route path='/' element={<Employee/>}/>
+            <Route path='/project' element={<Project/>}/>
+            <Route path='/task' element={<Task/>}/>
+
+          </Routes>
+
+
+        </div>
+      </Router>
   )
 }
 
--- src/component/Project.tsx
+++ src/component/Project.tsx
@@ -1,5 +1,5 @@
 function Project(){
-    return <h1>Home</h1>
+    return <h1>Project</h1>
 }
 
 export default Project;
